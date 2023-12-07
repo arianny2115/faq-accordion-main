11360ddfd551e0b94ae6d29a4a26ab4179ac8ed2
Arianny Ayres <ariannyayres8@gmail.com>
2023-12-07 16:22:32 -0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,68 @@
+function plus1(){
+    let paragrafo1 = window.document.getElementById('paragrafo1')
+    let button1p = window.document.getElementById('button1p')
+    let button1m = window.document.getElementById('button1m')
+    button1p.style.display = 'none'
+    button1m.style.display = 'block'
+    paragrafo1.style.display = 'block'
+
+}
+function minus1(){
+    let paragrafo1 = window.document.getElementById('paragrafo1')
+    let button1p = window.document.getElementById('button1p')
+    let button1m = window.document.getElementById('button1m')
+    button1p.style.display = 'block'
+    button1m.style.display = 'none'
+    paragrafo1.style.display = 'none'
+    
+}
+function plus2(){
+    let paragrafo2 = window.document.getElementById('paragrafo2')
+    let button2p = window.document.getElementById('button2p')
+    let button2m = window.document.getElementById('button2m')
+    button2p.style.display = 'none'
+    button2m.style.display = 'block'
+    paragrafo2.style.display = 'block'
+}
+function minus2(){
+    let paragrafo2 = window.document.getElementById('paragrafo2')
+    let button2p = window.document.getElementById('button2p')
+    let button2m = window.document.getElementById('button2m')
+    button2p.style.display = 'block'
+    button2m.style.display = 'none'
+    paragrafo2.style.display = 'none'
+}
+function plus3(){
+    let paragrafo3 = window.document.getElementById('paragrafo3')
+    let button3p = window.document.getElementById('button3p')
+    let button3m = window.document.getElementById('button3m')
+    button3p.style.display = 'none'
+    button3m.style.display = 'block'
+    paragrafo3.style.display = 'block'
+}
+function minus3(){
+    let paragrafo3 = window.document.getElementById('paragrafo3')
+    let button3p = window.document.getElementById('button3p')
+    let button3m = window.document.getElementById('button3m')
+    button3p.style.display = 'block'
+    button3m.style.display = 'none'
+    paragrafo3.style.display = 'none'
+}
+
+function plus4(){
+   let paragrafo4 = window.document.getElementById('paragrafo4')
+   let button4p = window.document.getElementById('button4p')
+   let button4m = window.document.getElementById('button4m')
+   button4m.style.display = 'block'
+    button4p.style.display = 'none'
+   paragrafo4.style.display = 'block'
+   
+}
+function minus4(){
+    let paragrafo4 = window.document.getElementById('paragrafo4')
+    let button4p = window.document.getElementById('button4p')
+    let button4m = window.document.getElementById('button4m')
+    button4m.style.display = 'none'
+    button4p.style.display = 'block'
+    paragrafo4.style.display = 'none'
+}
